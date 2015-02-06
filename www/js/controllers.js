2a675b8df6614f4b0ed9cde8c55e832b150c84b4
Joseph Le <josephle@me.com>
2015-02-05 20:50:55 -0800

    Restructured popup and added "Add Course"

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-  .controller('AppCtrl', function($scope, $ionicModal, $ionicPopup, $timeout) {
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
     // Form data for the login modal
     $scope.loginData = {};
 
@@ -31,7 +31,69 @@ angular.module('starter.controllers', [])
         $scope.closeLogin();
       }, 1000);
     };
+  })
 
+//.controller('PlaylistsCtrl', function($scope) {
+//  $scope.playlists = [
+//    { title: 'Reggae', id: 1 },
+//    { title: 'Chill', id: 2 },
+//    { title: 'Dubstep', id: 3 },
+//    { title: 'Indie', id: 4 },
+//    { title: 'Rap', id: 5 },
+//    { title: 'Cowbell', id: 6 }
+//  ];
+//})
+//
+//.controller('PlaylistCtrl', function($scope, $stateParams) {
+//})
+
+  .controller('CoursesCtrl', function($scope, $ionicPopup) {
+    $scope.addCourse = function() {
+      $scope.data = {};
+
+      // An elaborate, custom popup
+      var myPopup = $ionicPopup.show({
+        template: '<label for="dept">Class Department</label><input id="dept" type="text" ng-model="data.dept">' +
+        '<label for="code">Class Code</label><input id="code" type="text" ng-model="data.code">',
+        title: 'Add New Course',
+        scope: $scope,
+        buttons: [
+          {text: 'Cancel'},
+          {
+            text: '<b>Add</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+              if (!$scope.data.title && !$scope.data.class) {
+                //don't allow the user to close unless he enters wifi password
+                e.preventDefault();
+              }
+              else {
+                return {title: $scope.data.dept, class: $scope.data.code};
+              }
+            }
+          }
+        ]
+      });
+    };
+
+    $scope.courses = [
+      {id: 1, title: 'HILD 7B'},
+      {id: 2, title: 'COGS 120'},
+      {id: 3, title: 'COGS 187B'},
+      {id: 4, title: 'COGS 102C'}
+    ];
+  })
+
+  .controller('SharedListsCtrl', function($scope) {
+    $scope.lists = [
+      {id: 1, course: 'HILD 7B', uses: 67},
+      {id: 2, course: 'COGS 120', uses: 120},
+      {id: 3, course: 'COGS 187B', uses: 87},
+      {id: 4, course: 'COGS 102C', uses: 90}
+    ];
+  })
+
+  .controller('TasksCtrl', function($scope, $ionicPopup) {
     $scope.addTask = function() {
       $scope.data = {};
 
@@ -66,41 +128,7 @@ angular.module('starter.controllers', [])
         myPopup.close(); //close the popup after 3 seconds for some reason
       }, 3000);
     };
-  })
 
-//.controller('PlaylistsCtrl', function($scope) {
-//  $scope.playlists = [
-//    { title: 'Reggae', id: 1 },
-//    { title: 'Chill', id: 2 },
-//    { title: 'Dubstep', id: 3 },
-//    { title: 'Indie', id: 4 },
-//    { title: 'Rap', id: 5 },
-//    { title: 'Cowbell', id: 6 }
-//  ];
-//})
-//
-//.controller('PlaylistCtrl', function($scope, $stateParams) {
-//})
-
-  .controller('CoursesCtrl', function($scope) {
-    $scope.courses = [
-      {id: 1, title: 'HILD 7B'},
-      {id: 2, title: 'COGS 120'},
-      {id: 3, title: 'COGS 187B'},
-      {id: 4, title: 'COGS 102C'}
-    ];
-  })
-
-  .controller('SharedListsCtrl', function($scope) {
-    $scope.lists = [
-      {id: 1, course: 'HILD 7B', uses: 67},
-      {id: 2, course: 'COGS 120', uses: 120},
-      {id: 3, course: 'COGS 187B', uses: 87},
-      {id: 4, course: 'COGS 102C', uses: 90}
-    ];
-  })
-
-  .controller('TasksCtrl', function($scope) {
     $scope.tasks = [
       {id: 1, title: 'Essay', class: 'HILD 7B'},
       {id: 2, title: 'Storyboard', class: 'COGS 120'},
