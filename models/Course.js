c5e2db0ed226987dbbce0eb9d2cb3e0f42b47888
Joseph Le <josephle@me.com>
2015-02-18 15:11:42 -0800

    Added "shared" to schema

--- models/Course.js
+++ models/Course.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var courseSchema = new mongoose.Schema({
   name: String,
   tasks: [mongoose.Schema.Types.ObjectId],
+  shared: Boolean,
   createdBy: mongoose.Schema.Types.ObjectId,
   createdAt: { type: Date, default: Date.now }
 });
