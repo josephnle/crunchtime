846fda182e72ce93334169aeb88cf1a949a0ec30
Joseph Le <josephle@me.com>
2015-02-18 15:10:38 -0800

    Added course schema

---
+++ models/Course.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var courseSchema = new mongoose.Schema({
+  name: String,
+  tasks: [mongoose.Schema.Types.ObjectId],
+  createdBy: mongoose.Schema.Types.ObjectId,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Course', courseSchema);
